11329406d695b19f118acc4e966fdd48e4b7851c
alers16 <alerosan16@gmail.com>
2024-10-17 19:19:44 +0200

    Update RecevoirContext.jsx

--- frontend/src/context/RecevoirContext.jsx
+++ frontend/src/context/RecevoirContext.jsx
@@ -2,15 +2,15 @@ import { createContext, useContext, useEffect } from "react"
 import axios from "axios";
 
 const RecevoirContext = createContext(null);
-const SERVER_URL = "http://malackathon.yellowbeavers.com:6996"; // CAMBIAR A LOCALHOST EN EL SV
+const SERVER_URL = "http://malackathon.yellowbeavers.com:6998"; // CAMBIAR A LOCALHOST EN EL SV
 
 export const RecevoirProvider = ({ children }) => {
 
     const get = async (url) => await axios.get(SERVER_URL + url);
     const post = async (url, parameters) => await axios.post(SERVER_URL + url, parameters);
 
-    const getEmbalses = async (x, y, radius) => await get("/api/embalses?x=" + x + "&y=" + y + "&radius=" + radius);
-    const getAguaFromEmbalses = async (id) => await get("/api/embalses/" + id + "/agua");
+    const getEmbalses = async (x, y, radius) => await get("/embalses?x=" + x + "&y=" + y + "&radius=" + radius);
+    const getAguaFromEmbalses = async (id) => await get("/embalses/agua?id=" + id);
 
     return (
         <RecevoirContext.Provider value={{
