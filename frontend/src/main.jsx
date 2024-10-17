37430ec6a14c694d365e580e9b81a6074a6da9e4
antBaena <antoniocanetebaena1234@gmail.com>
2024-10-17 17:39:06 +0200

    Add header only in main page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,23 @@
-import { createRoot } from 'react-dom/client'
-import HomePage from './pages/HomePage.jsx'
-import RecevoirPage from './pages/RecevoirPage.jsx'
-import "bootstrap/dist/css/bootstrap.min.css"
-import './index.css'
-import HeaderPage from './components/HeaderComponent.jsx'
-import Footer from './components/FooterComponent.jsx'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import ResultPage from './pages/ResultPage.jsx'
-import { RecevoirProvider } from './context/RecevoirContext.jsx'
+import { createRoot } from "react-dom/client";
+import HomePage from "./pages/HomePage.jsx";
+import RecevoirPage from "./pages/RecevoirPage.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import HeaderPage from "./components/HeaderComponent.jsx";
+import Footer from "./components/FooterComponent.jsx";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import ResultPage from "./pages/ResultPage.jsx";
+import { RecevoirProvider } from "./context/RecevoirContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <RecevoirProvider>
-    <HeaderPage />
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/result' element={<ResultPage />} />
-        <Route path='/recevoir' element={<RecevoirPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/result" element={<ResultPage />} />
+        <Route path="/recevoir" element={<RecevoirPage />} />
       </Routes>
     </BrowserRouter>
     <Footer />
   </RecevoirProvider>
-)
+);
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,32 +1,44 @@
 import { act, useState } from "react";
 import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+import HeaderPage from "../components/HeaderComponent.jsx";
+export default function HomePage() {
+  const [actualLocation, setActualLocation] = useState([36.715103, -4.477658]);
 
-export default function HomePage(){
-
-    const [actualLocation, setActualLocation] = useState([36.715103,-4.477658]);
-
-    return <>
-        <main className="container">
-            <div className="row g-3">
-                <div className="col-6">
-                {"<mapa>"}
-                </div>
-                <div className="col-6">
-                    
-                    <div className="mb-3">
-                        <label for="locationInput" className="form-label">Ubicación</label>
-                        <input id="locationInput" className="form-control" type="search"></input>
-                    </div>
-                    <button className="btn btn-primary" onClick={() => {
-                        navigator.geolocation.getCurrentPosition((position) => {
-                            setActualLocation([position.coords.latitude, position.coords.longitude])
-                        })
-                    }}>Usar mi ubicación</button>
-                </div>
-                <div className="col-12">
-                </div>
-                Ubicación seleccionada: {actualLocation}
+  return (
+    <>
+      <HeaderPage />
+      <main className="container">
+        <div className="row g-3">
+          <div className="col-6">{"<mapa>"}</div>
+          <div className="col-6">
+            <div className="mb-3">
+              <label for="locationInput" className="form-label">
+                Ubicación
+              </label>
+              <input
+                id="locationInput"
+                className="form-control"
+                type="search"
+              ></input>
             </div>
-        </main>
+            <button
+              className="btn btn-primary"
+              onClick={() => {
+                navigator.geolocation.getCurrentPosition((position) => {
+                  setActualLocation([
+                    position.coords.latitude,
+                    position.coords.longitude,
+                  ]);
+                });
+              }}
+            >
+              Usar mi ubicación
+            </button>
+          </div>
+          <div className="col-12"></div>
+          Ubicación seleccionada: {actualLocation}
+        </div>
+      </main>
     </>
+  );
 }
