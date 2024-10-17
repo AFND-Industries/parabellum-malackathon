d618980e268aab118e0e49bea1389ef7f04b53dd
antbaena <antoniocanetebaena1234@gmail.com>
2024-10-17 14:39:51 +0000

    backend update

---
+++ backend/controllers/embalseController.js
@@ -0,0 +1,43 @@
+const axios = require('axios'); // Asegúrate de importar axiosAsegúrate de que estas funciones estén definidas
+const oracledb = require('oracledb');
+const dbConfig = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  connectString: process.env.DB_CONNECT_STRING,
+};
+
+
+
+
+
+const embsalseController = {};
+
+embsalseController.getByCoords = async (req, res) => {
+  let connection; // Declarar la conexión aquí para el finally
+  try {
+    const { x, y, radius } = req.query;
+
+    // Verificar que los parámetros existan
+    if (!x || !y || !radius) {
+      return res.status(400).json({ error: "Faltan parámetros: x, y y/o radius son requeridos." });
+    }
+
+    // Realiza una solicitud GET a tu API de FastAPI
+    const fastApiUrl = `http://malackathon.yellowbeavers.com:6996/api/embalses?x=${x}&y=${y}&radius=${radius}`; // URL de tu FastAPI
+    const fastApiResponse = await axios.get(fastApiUrl);
+
+    // Envía la respuesta de FastAPI a tu cliente
+    res.json(fastApiResponse.data);
+
+  } catch (error) {
+    console.error(error); // Log del error
+    res.status(500).json({ error: "Error al conectar con la base de datos o FastAPI" });
+  } finally {
+    // Cierra la conexión, solo si fue abierta
+    if (connection) {
+      await closeConnection(connection);
+    }
+  }
+};
+
+module.exports = embsalseController;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const embsalseController = require('../controllers/embalseController');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/embalse', embsalseController.getByCoords);
 
 module.exports = router;
