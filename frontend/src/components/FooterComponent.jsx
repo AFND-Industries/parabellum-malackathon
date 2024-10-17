e0402bbae404c9d5be623b64f7d79a7c26806ad6
antBaena <antoniocanetebaena1234@gmail.com>
2024-10-17 18:57:28 +0200

    Small update header/footer

--- frontend/src/components/FooterComponent.jsx
+++ frontend/src/components/FooterComponent.jsx
@@ -1,7 +1,7 @@
 import "./footer.css";
 export default function Footer() {
   return (
-    <div className="py-1 text-center">
+    <div className="py-1 text-center bg-dark">
       <h1 className="fs-6 text-white">Malackathon-2024</h1>
       <i>
         <b className="text-light">Para Bellum ⚔</b>
